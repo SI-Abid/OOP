28558735526b3860b6954ed9404e4babafbe6e8f
Saiham Islam <siaabid5323@gmail.com>
2021-07-05 23:04:47 +0600

    java scanner class

---
+++ J_ii.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class J_ii {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        
+        System.out.print("Enter your name: ");
+        System.out.print("Enter your age: ");
+        
+        System.out.println("Welcome to java programming, " + name);
+        System.out.println("Your are " + age + " years old.");
+        
+        scanner.close();
+    }
+}
