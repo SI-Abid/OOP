45203f755e51ebb28d333d36dac034e3bb2c38e9
Saiham Islam <siaabid5323@gmail.com>
2021-07-11 16:03:14 +0600

    linear search

--- Main.java
+++ Main.java
@@ -4,23 +4,29 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        
+
         Scanner scan = new Scanner(System.in);
 
-        int a, b;
-        a = scan.nextInt();
-        b = scan.nextInt();
-        
-        scan.nextLine();                    // skip newline 
-        String str = scan.nextLine();
-
-        int sum = a + b;
-        int mul = a * b;
-
-        System.out.println("sum = " + sum);
-        System.out.println("mul = " + mul);
-        System.out.println("str = " + str);
-
+        System.out.print("Enter array size: ");
+        int arraySize = scan.nextInt();
+        int[] array = new int[arraySize];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print(i + 1 + ": ");
+            array[i] = scan.nextInt();
+        }
+        System.out.print("Enter your item to be searched: ");
+        int item = scan.nextInt();
+        int pos = -1;
+        for (int i = 0; i < arraySize; i++) {
+            if (array[i] == item)
+                pos = i;
+        }
+        if (pos == -1) {
+            System.out.println("Item " + item + " not found");
+        } else {
+            System.out.println("Item " + item + "found at index " + pos);
+        }
         scan.close();
     }
 }
