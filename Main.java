3584eac2c3610e0dcad71e867f72916daa604f80
Saiham Islam <siaabid5323@gmail.com>
2021-07-26 23:47:22 +0600

    session one

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
     public static void main(String[] args) {
-        
+        System.out.print("Hello Everyone.\n");
+        System.out.println("Hello world. 2nd line.");
     }
 }
+
+/**
+ * create an object of the class
+ * object.method()
+ * 
+ * classname.method()
+ * 
+ * user: run Main.java
+ * OS: Main.main()
+ * 
+ * 
+ */
