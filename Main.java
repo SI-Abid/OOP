1fe325a963f042c7fa65d2bf2480d37b50d6194c
Saiham Islam <siaabid5323@gmail.com>
2021-07-26 23:51:43 +0600

    session one

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Hello Everyone.\n");
         System.out.println("Hello world. 2nd line.");
+        /**
+         * System.out.println(args[0]);
+         * System.out.println(args[1]);
+         * System.out.println(args[2]);
+         */
     }
 }
 
