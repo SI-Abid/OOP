683947cd2d749422b38a84f823b9ac77d28c0b47
Saiham Islam <siaabid5323@gmail.com>
2021-08-03 03:50:08 +0600

    Dijstkra

---
+++ J_xxv.java
@@ -0,0 +1,76 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+
+class Node {
+    int u;
+    int cost;
+    Node(int u, int cost) {
+        this.u = u;
+        this.cost = cost;
+    }
+    public boolean lessthan(Node that) {
+        return this.cost < that.cost;
+    }
+}
+
+class Dijstkra {
+    public static void spt(int n, int[] adj[], int[] cost[], int s) {
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue <Node> pq = new PriorityQueue<Node>();
+        pq.add(new Node(s, 0));
+        distance[s] = 0;
+
+        while (!pq.isEmpty()) {
+            Node top = pq.poll();
+            int u = top.u;
+
+            for(int i = 0; i < adj[u].length; i++) {
+                int v = adj[u][i];
+
+                if (distance[v] > distance[u] + cost[u][i]) {
+                    distance[v] = distance[u] + cost[u][i];
+                    pq.add(new Node(v, distance[v]));
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++) {
+            System.out.println(s+"-->"+distance[i]);
+        }
+    }
+}
+
+
+public class J_xxv {
+    public static void main(String[] args) {
+        File file = new File("in.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            int node = scanner.nextInt();
+            int edge = scanner.nextInt();
+            
+            int[][] adj = new int[node+1][node+1];
+            int[][] cost = new int[node+1][node+1];
+
+            for(int i = 0; i < edge; i++) {
+                int u = scanner.nextInt();
+                int v = scanner.nextInt();
+                int w = scanner.nextInt();
+                adj[u][v] = 1;
+                adj[v][u] = 1;
+                cost[u][v] = w;
+                cost[v][u] = w;
+            }
+            int s = 1;
+            Dijstkra.spt(node, adj, cost, s);
+
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        String s = sc.next();
-
-        if(Character.isLowerCase(s.charAt(0)))
-        System.out.println(Character.toUpperCase(s.charAt(0))+s.substring(1));
-
-        sc.close();
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        String[] words = line.split("");
+        for (String string : words) {
+            System.out.println(string);
+        }
+        int i=0;
+        do {
+            i++;
+        }while(i<10);
+        in.close();
     }
 }
 // +s.substring(1)
