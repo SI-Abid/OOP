6c86d58d81ef968cd8afe30265979eccafdd7439
Saiham Islam <siaabid5323@gmail.com>
2021-08-07 16:45:38 +0600

    array sum and static method

--- Main.java
+++ Main.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * Main
+ */
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        String[] words = line.split("");
-        for (String string : words) {
-            System.out.println(string);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
         }
-        int i=0;
-        do {
-            i++;
-        }while(i<10);
-        in.close();
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = scan.nextInt();
+        }
+        int asum = 0, bsum = 0;
+        for (int i = 0; i < n; i++) {
+            asum += a[i];
+            bsum += b[i];
+        }
+        System.out.println(abs(asum - bsum));
+        scan.close();
+    }
+    static int abs(int a) {
+        return a < 0 ? -a : a;
     }
 }
-// +s.substring(1)
