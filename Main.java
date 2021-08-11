51acf3a10bcf58ce2171c513e6148a54ecf3d4d9
Saiham Islam <siaabid5323@gmail.com>
2021-08-11 21:39:43 +0600

    assignment 2

--- Main.java
+++ Main.java
@@ -1,30 +1,72 @@
 import java.util.Scanner;
 
+class Text {
+    String text;
+    String[] lines;
+    int lineCount;
+    String[] words;
+    int wordCount;
+    int[] vowelsPerLine;
+    int oddLines;
+    int oddWords;
+
+    public Text(String text) {
+        this.text = text;
+        // Split text into lines if theres a character . , ? !" etc.
+        lines = text.split("[\\.\\,\\?\\!\\\"\\']");
+        lineCount = lines.length;
+        words = text.split("\\s");
+        wordCount = words.length;
+        vowelsPerLine = new int[lineCount];
+        oddLines = 0;
+        oddWords = 0;
+        for (int i = 0; i < lineCount; i++) {
+            vowelsPerLine[i] = countVowels(lines[i]);
+            if (vowelsPerLine[i] % 2 != 0) {
+                oddLines++;
+            }
+        }
+        for (int i = 0; i < wordCount; i++) {
+            if (words[i].length() % 2 != 0) {
+                oddWords++;
+            }
+        }
+    }
+    public String result() {
+        StringBuilder sb = new StringBuilder();
+        for (int vpl : vowelsPerLine) {
+            sb.append(" ").append(vpl);
+        }
+        return "Text: " + text + "\n" +
+                "Lines: " + lineCount + "\n" +
+                "Words: " + wordCount + "\n" +
+                "Vowels per line:" + sb.toString() + "\n" + 
+                "Odd lines: " + oddLines + "\n" +
+                "Odd words: " + oddWords;
+    }
+    private int countVowels(String line) {
+        int vowels = 0;
+        for (int i = 0; i < line.length(); i++) {
+            char c = Character.toLowerCase(line.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ) {
+            vowels++;
+            }
+        }
+        return vowels;
+    }
+}
 /**
  * Main
  */
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scan.nextInt();
-        }
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++) {
-            b[i] = scan.nextInt();
-        }
-        int asum = 0, bsum = 0;
-        for (int i = 0; i < n; i++) {
-            asum += a[i];
-            bsum += b[i];
-        }
-        System.out.println(abs(asum - bsum));
-        scan.close();
-    }
-    static int abs(int a) {
-        return a < 0 ? -a : a;
+        Scanner in = new Scanner(System.in);
+ 
+        System.out.println("Input:");
+        String text = in.nextLine();
+        Text t = new Text(text);
+        System.out.println("Output:\n"+t.result());
+
+        in.close();
     }
 }
