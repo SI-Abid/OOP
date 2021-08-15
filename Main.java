9215f9e109a7931f28afa6af16788b77dc855b96
Saiham Islam <siaabid5323@gmail.com>
2021-08-15 23:10:57 +0600

    updated

--- Main.java
+++ Main.java
@@ -1,28 +1,13 @@
 import java.util.Scanner;
 
-
-class Basa {
-
-    static void printBasa() {
-        System.out.println("Basa");
-    }
-    class Room {
-
-        static void print() {
-        
-            System.out.println("Ghorar Dim");
-        
-        }
-    }
+class Bus {
+    String name, dest, from;
+    
+    
 }
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner in = new Scanner(System.in);
- 
-        Basa.printBasa();
-        Basa.Room.print();
-
-        // in.close();
+        
     }
 }
