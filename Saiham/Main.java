ea927717e23edd952bc026b88ff098a8ee6d99e5
Saiham Islam <siaabid5323@gmail.com>
2021-09-28 10:05:21 +0600

    updated

--- Saiham/Main.java
+++ Saiham/Main.java
@@ -1,24 +1,32 @@
 package Saiham;
 
+class Trash {
+    public final static int MAX = 1000000000;
+}
+
+interface CONST {
+
+    public static int MAX = 2000000000;
+
+    public void bodyLess();
+
+}
+
+class Body implements CONST {
+    public void bodyLess() {
+        System.out.println("Matha ekjagat\nBody arek jagat");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         
-        Student s1 = new Student();
+        System.out.println("Hello World");
+        int limit = Trash.MAX;
+        int lower_bound = MIN;
+        System.out.println(CONST.MAX);
+        limit = limit + lower_bound;
 
-        Student s2 = s1.clone();
-        Student s3 = s1.clone();
-
-        s1.printInfo();
-        s2.printInfo();
-        s3.printInfo();
-
-        s2.department = "CSE";
-        s3.name = "Sukanto";
-
-        System.out.println("------------------------------");
-        
-        s1.printInfo();
-        s2.printInfo();
-        s3.printInfo();
     }
+    public final static int MIN = -Trash.MAX;
 }
