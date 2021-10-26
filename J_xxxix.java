3cfc741e10653b203d40cb43b75d7f024a1620e8
Saiham Islam <siaabid5323@gmail.com>
2021-10-26 09:36:23 +0600

    java enum

---
+++ J_xxxix.java
@@ -0,0 +1,14 @@
+enum Mango {
+    // these are by-default public static final type
+    Surjapuri, Ashwina, Kalia, Deori, Fazli, Langra, Gopalbhog, Bombai, Khirsapat
+}
+public class J_xxxix {
+    public static void main(String[] args) {
+        
+        for (Mango mango : Mango.values()) {
+            System.out.println(mango);
+        }
+        //test enum 
+        
+    }
+}
